40780222748ebb90a614fcdaa3de3991adaa8992
nick.812 <nik.jeromel@student.um.si>
2023-03-19 11:16:13 +0100

    added dvc

--- src/data/fetch_data.py
+++ src/data/fetch_data.py
@@ -7,7 +7,7 @@ response_API = requests.get('https://arsoxmlwrapper.app.grega.xyz/api/air/archiv
 
 url = "https://meteostat.p.rapidapi.com/stations/hourly"
 
-querystring = {"station":"14015","start":"2023-02-15","end":str(date.today()),"tz":"Europe/Berlin"}
+querystring = {"station":"14015","start":"2023-02-20","end":str(date.today()),"tz":"Europe/Berlin"}
 
 headers = {
 	"X-RapidAPI-Key": "8b10ab09bbmshc60e103331db811p1d4863jsna06aab62b046",
@@ -17,6 +17,7 @@ headers = {
 response = requests.request("GET", url, headers=headers, params=querystring)
 
 print(response.text)
+
 
 with open('data/raw/weather/neobdelani.json', 'w') as f:
     f.write(response.text)
